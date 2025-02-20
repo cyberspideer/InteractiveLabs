ae6be44c5bb45a63ef6d2d16d0b6e40df8b80670
cyberspideer <zainabideen.pucad@gmail.com>
2025-02-20 20:04:12 +0200

    Lab 3.
    Class exercise data.

---
+++ Lab3/logic.js
@@ -0,0 +1,28 @@
+const themeBtn = document.querySelector ("#theme-toggle");
+const modeLight = document.querySelector ("#mode-light");
+const modeDark = document.querySelector ("#mode-dark");
+
+let currentTheme = 'dark'
+const body = document.body;
+
+themeBtn.addEventListener("click",()=>{
+    console.log("Button is being clicked")
+    body.classlist.toggle ("dark-mode");
+
+    if(currentTheme === "dark")
+    {
+        // Show light icon
+        modeDark.style.display = "none";
+        modeLight.style.display = "inline";
+        localStorage.setItem("currentTheme","dark");
+        // currentTheme = "light"
+    }
+    else
+    {
+        modeLight.style.display = "none";
+        modeDark.style.display = "inline";
+        // show dark icon switch BG colors
+        localStorage.setItem("currentTheme" ,"light");
+        currentTheme = "light"
+    }
+})
