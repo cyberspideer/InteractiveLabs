42414ad4f867b325c0671d0f254d07316d5d3015
cyberspideer <zainabideen.pucad@gmail.com>
2025-03-20 17:48:58 +0200

    HomeWork5 TicTacToe
    Base Version

---
+++ HomeWork5/my-vite-app/src/components/Game.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import Board from './Board';
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [Array(9).fill(null)],
+      stepNumber: 0,
+      xIsNext: true,
+    };
+  }
+
+  handlePlay(index) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const currentSquares = history[history.length - 1].slice();
+    
+    if (currentSquares[index] || calculateWinner(currentSquares)) return;
+
+    currentSquares[index] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([currentSquares]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0,
+    });
+  }
+
+  render() {
+    const history = this.state.history;
+    const currentSquares = history[this.state.stepNumber];
+    const winner = calculateWinner(currentSquares);
+
+    let status;
+    if (winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    }
+
+    const moves = history.map((_, move) => {
+      const desc = move ? `Go to move #${move}` : 'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board squares={currentSquares} onPlay={(i) => this.handlePlay(i)} />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ];
+  for (let [a, b, c] of lines) {
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+export default Game;
